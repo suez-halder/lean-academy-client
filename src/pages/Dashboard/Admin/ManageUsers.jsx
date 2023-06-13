2e81d441eded1adf3e524042bfb987420d9bd501
suezhalder <szhldr@gmail.com>
2023-06-13 15:08:43 +0600

    manage users page done

--- src/pages/Dashboard/Admin/ManageUsers.jsx
+++ src/pages/Dashboard/Admin/ManageUsers.jsx
@@ -1,13 +1,32 @@
+import toast from "react-hot-toast";
 import useUsers from "../../../hooks/useUsers";
 
 const ManageUsers = () => {
 
-    const [users] = useUsers()
+    const [users, refetch] = useUsers()
     // console.log(users);
 
-    const filteredUsers = users?.filter(user => user.role !== 'admin') || [];
+    const filterAdminUser = users?.filter(user => user.email !== `${import.meta.env.VITE_SUPER_ADMIN}`)
 
+    const updateUserRole = (user, newRole) =>{
+        fetch(`${import.meta.env.VITE_API_URL}/users/role/${user._id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ role: newRole })
+        })
 
+        .then(res=>res.json())
+        .then(data=>{
+            // console.log(data);
+            if(data.modifiedCount){
+                refetch();
+                toast.success("Role Updated Successfully")
+                
+            }
+        })
+    }
 
 
     return (
@@ -19,21 +38,34 @@ const ManageUsers = () => {
                         <th>#</th>
                         <th>Name</th>
                         <th>Email</th>
+                        <th>Current Role</th>
                         <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {/* row 1 */}
                     {
-                        filteredUsers.map((user, index) => <tr
+                        filterAdminUser.map((user, index) => <tr
                             key={user._id}
                         >
                             <th>{index + 1}</th>
                             <td>{user.name}</td>
                             <td>{user.email}</td>
+                            <td className="uppercase">{user.role}</td>
                             <td>
-                                <button className="btn">Admin</button>
-                                <button className="btn">Instructor</button>
+                                <button 
+                                onClick={() => updateUserRole(user, 'admin')}  
+                                className="btn btn-error"
+                                disabled={user.role === 'admin'}
+                                >Admin
+                                
+                                </button>
+                                
+                                <button onClick={() => 
+                                    updateUserRole(user, 'instructor')} 
+                                className="btn btn-warning"
+                                disabled={user.role === 'instructor'}
+                                >Instructor</button>
                             </td>
                         </tr>)
                     }
