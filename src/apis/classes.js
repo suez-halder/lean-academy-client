eed719e693ec6853d0cbc634dec61c844921137b
suezhalder <szhldr@gmail.com>
2023-06-13 19:34:12 +0600

    addClass page complete

---
+++ src/apis/classes.js
@@ -0,0 +1,20 @@
+// add a class
+export const addClass = async classData => {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/classes`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(classData),
+    })
+  
+    const data = await response.json()
+    return data
+  }
+
+// get all classes
+export const getAllClasses = async () =>{
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/classes`)
+      const data = await response.json()
+      return data
+}
--- src/pages/Dashboard/Instructor/AddClass.jsx
+++ src/pages/Dashboard/Instructor/AddClass.jsx
@@ -1,14 +1,52 @@
 import { useForm } from "react-hook-form";
+import { addClass } from "../../../apis/classes";
+import { imageUpload } from "../../../apis/utils";
 import useAuth from "../../../hooks/useAuth";
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 const AddClass = () => {
-    const {user} = useAuth()
-    console.log(user);
+    const {user, setLoading} = useAuth()
+    // console.log(user);
+    const navigate = useNavigate()
+    
 
     const { register, handleSubmit, formState: { errors } } = useForm();
     
     const handleAddClass = data =>{
-        console.log(data);
+        setLoading(true)
+        // console.log(data.className);
+        const className= data.className
+        const seats = parseInt(data.seats)
+        const price = parseFloat(data.price)
+        // console.log(data.classImage);
+
+        imageUpload(data.classImage[0])
+        .then(data=>{
+            const classData = {
+                className,
+                image: data.data.display_url,
+                name: user.displayName,
+                email: user.email,
+                seats,
+                price,
+                status: 'pending'
+            }
+            addClass(classData)
+            .then(data=>{
+                console.log(data);
+                setLoading(false)
+                toast.success('Class Added') 
+                navigate('/dashboard/all-classes')
+                
+            })
+            setLoading(false)
+        })
+
+
+        
+        
+        
     }
     
     return (
@@ -49,7 +87,13 @@ const AddClass = () => {
                         <label className="label">
                             <span className="label-text">Available Seats</span>
                         </label>
-                        <input type="number"  {...register("seats", { required: true })} name="seats" className="input input-bordered" />
+                        <input type="number"  {...register("seats", { required: true })} name="seats" className="input input-bordered" placeholder="5"/>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Price</span>
+                        </label>
+                        <input type="number"  {...register("price", { required: true })} name="price" className="input input-bordered" placeholder="$50"/>
                     </div>
                     
                     <div className="form-control mt-6 w-1/2 mx-auto">
