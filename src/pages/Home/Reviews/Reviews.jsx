4f7669fa62f7dcdd27f5164fa6c83e4ea66d3e3a
suezhalder <szhldr@gmail.com>
2023-08-10 12:58:32 +0600

    reviews section done

--- src/pages/Home/Reviews/Reviews.jsx
+++ src/pages/Home/Reviews/Reviews.jsx
@@ -5,21 +5,27 @@ const Reviews = () => {
         <div>
             <Heading
                 title={'Our Course Reviews'}
-                subtitle="These are the list of our top 6 instructors based on the number of enrolled student in the class. Choose your instructor now to develop your physical health!"
+                subtitle="Hear directly from our students. Genuine feedback on how our courses have impacted their learning journeys, ambitions, and professional growth."
                 center={true}
             ></Heading>
 
-            <div>
-
+            <div className="mt-12">
                 <div className="w-3/4 mx-auto flex">
-                    <img src="https://i.ibb.co/qkvPDd0/happy-Face.png" className="w-1/6 mx-auto rounded-full shadow-2xl" />
-
+                    <img src="https://i.ibb.co/rfvGnck/ivana.png" className="w-1/6 mx-auto rounded-full shadow-2xl" />
                     <div className="ml-8">
-                        <p className="py-6 text-sm text-slate-500 md:text-lg">Toy Athlete is one of the most exclusive Sports Toy shop in the wold, where you can find all product for your baby that your want to buy for your baby. I recommend this shop all of you</p>
-                        <h1 className="text-2xl font-bold">Alan Smith</h1>
-                        <p><small className=" text-slate-500">- CEO, Momens Group</small></p>
+                        <p className="py-6 text-sm text-slate-500 md:text-lg">"Before joining Lean Academy, I felt overwhelmed with the sheer amount of information out there. Their structured approach and top-notch instructors made all the difference in my learning journey. Not only did I gain practical knowledge, but I also developed a solid foundation that's serving me well in my professional life. Highly recommended for anyone looking to up their game!"</p>
+                        <h1 className="text-2xl font-bold mb-2">Lecia Savitski</h1>
+                        <p><small className=" text-slate-500 text-base">- Finance Advisor at J.P. Morgan</small></p>
                     </div>
-                    <hr />
+                </div>
+                <hr className="font-bold w-3/4 mx-auto my-8"/>
+                <div className="w-3/4 mx-auto flex">
+                    <img src="https://i.ibb.co/xJPDsrj/user-1.png" className="w-1/6 mx-auto rounded-full shadow-2xl" />
+                    <div className="ml-8">
+                        <p className="py-6 text-sm text-slate-500 md:text-lg">"I recently completed a course at Lean Academy, and I must say, it exceeded my expectations. The curriculum was clear, comprehensive, and up-to-date. While there's always room for improvement, especially in the realm of hands-on projects, the insights I gained here are invaluable. Kudos to the team for such an enriching experience!"</p>
+                        <h1 className="text-2xl font-bold mb-2">Pauly Garita</h1>
+                        <p><small className=" text-slate-500 text-base">- IT Recruiter at EX2 Outcoding</small></p>
+                    </div>
                 </div>
 
             </div>
